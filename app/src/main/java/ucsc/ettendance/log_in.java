a5bad94628817fc80fc9eb777fa72d0c34042860
Joey Aronson <jyaronso@ucsc.edu>
2018-01-17 13:27:29 -0800

    init

--- app/src/main/java/ucsc/ettendance/log_in.java
+++ app/src/main/java/ucsc/ettendance/log_in.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class log_in extends AppCompatActivity {
-        //init
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
