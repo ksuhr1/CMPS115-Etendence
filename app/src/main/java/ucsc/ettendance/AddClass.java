b90cc2db2ed587773f44d3b29930aa17f9fdebf5
ksuhr1 <ksuhr1@me.com>
2018-01-24 20:19:11 -0800

    katy init

--- app/src/main/java/ucsc/ettendance/AddClass.java
+++ app/src/main/java/ucsc/ettendance/AddClass.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class AddClass extends AppCompatActivity {
-
+//katy
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
