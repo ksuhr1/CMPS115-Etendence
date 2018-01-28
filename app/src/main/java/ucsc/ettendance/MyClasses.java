0abd5b5f1967421236853e2322f875ed6fb705bb
Joey Aronson <jyaronso@ucsc.edu>
2018-01-27 17:09:43 -0800

    small edits to login page

--- app/src/main/java/ucsc/ettendance/MyClasses.java
+++ app/src/main/java/ucsc/ettendance/MyClasses.java
@@ -1,8 +1,10 @@
 package ucsc.ettendance;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,20 +14,32 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MyClasses extends AppCompatActivity {
 
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
+
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    private static final String TAG = "My Classes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_classes);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // if user is null launch login activity
+                    startActivity(new Intent(MyClasses.this, LoginActivity.class));
+                    finish();
+                }else{
+                    Log.d(TAG,"HELLO: "+user.getEmail());
+                }
+            }
+        };
 
-        if(mFirebaseAuth == null) {
-            loadLogInView();
-        }
 
         //ADD CLASS BUTTON
         Button addClass = (Button) findViewById(R.id.addClassButton);
@@ -38,13 +52,4 @@ public class MyClasses extends AppCompatActivity {
             }
         });
     }
-
-    private void loadLogInView() {
-        Intent intent = new Intent(this, LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
-
-
 }
