5a35dae266073787ab16ddf99a3e3a703fb083ce
Joey Aronson <jyaronso@ucsc.edu>
2018-01-27 21:44:40 -0800

    test push

--- app/src/main/java/ucsc/ettendance/SignUp.java
+++ app/src/main/java/ucsc/ettendance/SignUp.java
@@ -1,23 +1,13 @@
 package ucsc.ettendance;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
+
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
