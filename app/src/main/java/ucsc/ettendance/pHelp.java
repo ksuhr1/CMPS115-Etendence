538b02e7fa78dfc75a1f06013c019fbface381fe
Joey Aronson <jyaronso@ucsc.edu>
2018-03-11 19:52:44 -0700

    help pages completed

--- app/src/main/java/ucsc/ettendance/pHelp.java
+++ app/src/main/java/ucsc/ettendance/pHelp.java
@@ -2,12 +2,34 @@ package ucsc.ettendance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class pHelp extends AppCompatActivity {
+    private ArrayAdapter<String> aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_p_help);
+
+        ListView help = (ListView) findViewById(R.id.listview);
+        final ArrayList<String> steps = new ArrayList<>();
+
+        steps.add("STEP 1: \nCreate the class with the recommended fields.");
+        steps.add("STEP 2: \nSelect the preferred class and navigate to the day to which you would like to take attendance.");
+        steps.add("STEP 3: \nSet an attendance code for the students to input for that certain day.");
+        steps.add("STEP 4: \nSet the location of the classroom on the map.");
+        steps.add("STEP 5: (Optional) \nClick “Present Students” to view students who were present for the selected day.");
+        steps.add("STEP 6: (Optional) \nClick “Absent Students” to view students who were absent for the selected day.");
+        steps.add("STEP 7: (Optional) \nClick “Announcements” to view your announcements.");
+        steps.add("STEP 8: (Optional) \nClick “Create Announcement” to add a new announcement.");
+
+
+
+        aa = new ArrayAdapter<String>(pHelp.this, R.layout.studentlistblue, steps);
+        help.setAdapter(aa);
     }
 }
--- app/src/main/java/ucsc/ettendance/studentHelp.java
+++ app/src/main/java/ucsc/ettendance/studentHelp.java
@@ -2,12 +2,32 @@ package ucsc.ettendance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class studentHelp extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class studentHelp extends AppCompatActivity
+{
+    private ArrayAdapter<String> aa;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_help);
+
+        ListView help = (ListView) findViewById(R.id.listview);
+        final ArrayList<String> steps = new ArrayList<>();
+
+        steps.add("STEP 1: \nAdd any class for the quarter by pressing the “Add Class” button.");
+        steps.add("STEP 2: \nEnter the class code and class pin given by the professor.");
+        steps.add("STEP 3: \nClick on a class to navigate to the class page.");
+        steps.add("STEP 4: \nTo check in for the day click the “Check In” button.");
+        steps.add("STEP 5: \nMake sure your location services so the professor knows that you are in the classroom.");
+        steps.add("STEP 6: \nInput the daily attendance code provided by the professor and click “Check In”.");
+
+        aa = new ArrayAdapter<String>(studentHelp.this, R.layout.helplist, steps);
+        help.setAdapter(aa);
     }
 }
